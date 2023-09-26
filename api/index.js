c2b518cb8844d0b46acd2348723eb7024cc3b5b4
AndresRangelMeli <andres.rangel@mercadolibre.com.co>
2023-09-26 13:36:06 -0500

    create connection mongo DB and models

---
+++ api/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
+
+const app = express();
+const port = 8000;
+const cors = require("cors");
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const jwt = require("jsonwebtoken");
+
+mongoose.connect("mongodb+srv://AndressRangell:andres123@cluster0.bhbj3w5.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((error) => {
+    console.log("Error connecting to MongoDB", error)
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
+
